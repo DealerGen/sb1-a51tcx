94fedf0b37d60e50a33cff97f27375ccc2390b1a
DealerGen <daniel@dealergen.ai>
2024-10-21 22:18:20 +0100

    Delete sb1-a51tcx (13) directory

--- sb1-a51tcx (13)/src/suppressWarnings.ts
+++
@@ -1,8 +0,0 @@
-const originalConsoleError = console.error;
-
-console.error = function(...args) {
-  if (typeof args[0] === 'string' && args[0].includes('Warning: %s: Support for defaultProps will be removed from memo components')) {
-    return;
-  }
-  originalConsoleError.apply(console, args);
-};
